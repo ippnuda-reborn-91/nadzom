bcc1f54d269433ab33bd054573c89193a0124c7c
ippnuda-reborn <muhammadyasykur26@gmail.com>
2026-03-11 16:57:23 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,13 +1,13 @@
-const CACHE_NAME = 'nadzom-v4';
+const CACHE_NAME = 'ippnuda-ramadhan-v6';
 const ASSETS = [
   './',
   './index.html',
   './manifest.json',
-  './Nadzom%20luru%20ilmu.mp3' // Ganti dengan nama file MP3 yang Bapak upload tadi
+  './Fenomena%2010%20Hari%20Terakhir%20Bulan%20Ramadhan%20.mp3' 
 ];
 
-self.addEventListener('install', (event) => {
-  event.waitUntil(
+self.addEventListener('install', (e) => {
+  e.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
       return cache.addAll(ASSETS);
     })
@@ -15,10 +15,24 @@ self.addEventListener('install', (event) => {
   self.skipWaiting();
 });
 
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => {
+      return res || fetch(e.request);
     })
   );
 });
